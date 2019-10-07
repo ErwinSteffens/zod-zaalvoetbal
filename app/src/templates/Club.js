54e93ce7bf4981660dce67dab53cc31dad847949
Erwin Steffens <erwin@zuludesk.com>
2019-10-07 08:05:02 +0200

    Update casing for templates and layout

---
+++ app/src/templates/Club.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import { graphql, Link } from 'gatsby'
+import { Helmet } from 'react-helmet'
+
+import Layout from '../components/Layout'
+import ClubLogo from '../components/ClubLogo'
+
+export default ({ data }) => {
+    const club = data.clubJson
+
+    const teams = club.teams.sort((a, b) => {
+        if (a.sortId < b.sortId) {
+            return -1
+        }
+        if (a.sortId > b.sortId) {
+            return 1
+        }
+        return 0
+    })
+
+    return (
+        <Layout className="club-page">
+            <Helmet>
+                <title>ZOD Zaalvoetbal - {club.name}</title>
+            </Helmet>
+            <div className="clearfix">
+                <ClubLogo className="page-header" club={club} />
+                <h3>{club.name}</h3>
+                <b>Contactpersoon:</b>
+                <br />
+                Erwin Steffens
+                <br />
+                Tel: 06-48482334
+                <br />
+                <br />
+            </div>
+            <h4>Teams</h4>
+
+            <ul className="team-list">
+                {teams.map(team => {
+                    return (
+                        <li>
+                            <Link key={team.id} as={Link} to={`/${club.id}/${team.name}`}>
+                                <ClubLogo club={club} className="small mr-2" />
+                                {team.fullName}
+                            </Link>
+                        </li>
+                    )
+                })}
+            </ul>
+        </Layout>
+    )
+}
+
+export const query = graphql`
+    query($id: String!) {
+        clubJson(id: { eq: $id }) {
+            id
+            name
+            teams {
+                id
+                name
+                fullName
+                sortId
+            }
+        }
+    }
+`
---
+++ app/src/templates/Location.js
@@ -0,0 +1,96 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import { List } from 'immutable'
+import { Row, Col } from 'react-bootstrap'
+import moment from 'moment'
+import { Helmet } from 'react-helmet'
+
+import Layout from '../components/Layout'
+import Games from '../components/Games'
+
+export default ({ data }) => {
+    const location = data.locationJson
+
+    const games = List(location.games)
+        .groupBy(game => {
+            return moment(game.time)
+                .startOf('day')
+                .toDate()
+        })
+        .sortBy((_v, k) => k)
+
+    const mapsUrl = `https://www.google.com/maps/embed/v1/place?q=place_id:${location.placeId}&key=AIzaSyCzh-2XRTB_MQdXyBPpEXQkyAxQz_FOibY`
+
+    return (
+        <Layout>
+            <Helmet>
+                <title>ZOD Zaalvoetbal - {location.venue}</title>
+            </Helmet>
+            <Row className="location-info mb-4">
+                <Col xs={12} md={6}>
+                    <h3>{location.venue}</h3>
+                    <b>Adres:</b>
+                    <br />
+                    {location.address}
+                    <br />
+                    {location.postalCode} {location.city}
+                    <br />
+                    <br />
+                </Col>
+                <Col xs={12} md={6}>
+                    <iframe
+                        title={location.venue}
+                        className="location-map"
+                        frameBorder="0"
+                        style={{ border: 0 }}
+                        src={mapsUrl}
+                        allowFullScreen
+                    ></iframe>
+                </Col>
+            </Row>
+
+            <h4 className="mb-4">Wedstrijden</h4>
+            {games.entrySeq().map(([date, games]) => {
+                return <Games key={date} date={date} games={games} />
+            })}
+        </Layout>
+    )
+}
+
+export const query = graphql`
+    query($id: String!) {
+        locationJson(id: { eq: $id }) {
+            id
+            venue
+            address
+            postalCode
+            city
+            placeId
+            games {
+                id
+                time
+                location {
+                    id
+                }
+                homeTeam {
+                    id
+                    name
+                    fullName
+                    club {
+                        id
+                        name
+                    }
+                }
+                awayTeam {
+                    id
+                    name
+                    fullName
+                    club {
+                        id
+                        name
+                    }
+                }
+            }
+        }
+    }
+`
