5b272f55727188917a8be7af2f90614df8b1c14e
Erwin Steffens <erwin@digitalbrain.com>
2022-11-17 20:29:08 +0100

    Change warning msg text

--- app/src/components/TemporaryWarning.tsx
+++ app/src/components/TemporaryWarning.tsx
@@ -7,7 +7,7 @@ const TemporaryWarning = () => {
       <Col xs={12}>
         <Alert className="text-center" variant="warning">
           Let op! Het kan zijn dat er in de komende periode nog wat kleine
-          wijzigingen plaats vinden in het schema.
+          wijzigingen plaats vinden in het speelschema.
         </Alert>
         <br />
       </Col>
