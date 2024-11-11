f2d66cbb157a87dda1d79f01d4a539f9326750a7
Erwin Steffens <erwin.steffens@jamf.com>
2024-11-11 08:00:07 +0100

    Fix typo

--- app/src/components/TemporaryWarning.tsx
+++ app/src/components/TemporaryWarning.tsx
@@ -7,7 +7,7 @@ const TemporaryWarning = () => {
       <Col xs={12}>
         <Alert className="text-center" variant="danger">
           <strong>Let op!</strong> Het kan zijn dat er in de komende periode nog
-          wijzigingenplaats vinden in de poules of het speelschema!
+          wijzigingen plaats vinden in de poules of het speelschema!
         </Alert>
         <br />
       </Col>
