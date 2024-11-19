3ce23aaf7d83652f17e4c7fb827431112e10a770
Erwin Steffens <erwin.steffens@jamf.com>
2024-11-19 08:07:45 +0100

    Update temporary msg

--- app/src/components/TemporaryWarning.tsx
+++ app/src/components/TemporaryWarning.tsx
@@ -6,13 +6,11 @@ const TemporaryWarning = () => {
     <Row>
       <Col xs={12}>
         <Alert className="text-center" variant="danger">
-          <p>
-            <strong>Let op!</strong> Het kan zijn dat er in de komende periode
-            nog wijzigingen plaats vinden in de poules of het speelschema!
-          </p>
-          <p>
-            Houd de website goed in de gaten voor de meest actuele informatie.
-          </p>
+          <strong>Let op!</strong> Het kan zijn dat er in de komende periode nog
+          wijzigingen plaats vinden in de poules of het speelschema!
+          <br />
+          <br />
+          Houd de website goed in de gaten voor de meest actuele informatie.
         </Alert>
         <br />
       </Col>
